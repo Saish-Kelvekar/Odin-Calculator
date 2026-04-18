6aac99cc44284b47d2fdc1065aa3eea1447af5d7
Saish-Kelvekar <kelvekarsaish@gmail.com>
2026-04-18 11:11:16 +0530

    Added the complete logic for the working of the calculator and also added some style to the calc
        used 3 buttons and used .dataset.type to know the type and
        segregated the button click on that basis

--- script.js
+++ script.js
@@ -1,17 +1,72 @@
-const add=(a,b)=>{return a+b;};
-const sub=(a,b)=>{return a-b};
-const mul=(a,b)=>{return a*b};
-const div=(a,b)=>{return a/b};
+const add=(a,b)=>{return Number(a)+Number(b);};
+const sub=(a,b)=>{return Number(a)-Number(b);};
+const mul=(a,b)=>{return Number(a)*Number(b);};
+const div=(a,b)=>{return Number(a)/Number(b);};
 
 function operate(opr,a,b){
     switch(opr){
         case "+":return add(a,b);
         case "-":return sub(a,b);
-        case "*":return mul(a,b);
-        case "/":if(b==0){
+        case "×":return mul(a,b);
+        case "÷":if(Number(b)==0){
                 alert("cannot divide by 0");
+                return;
                 }
                 return div(a,b);
-        default:alert("unrecognized operation")
+        default:alert("unrecognized operation");
     }
 }
+
+function work(){
+    let firstNumber="";
+    let operator="";
+    let secondNumber="";
+    const display=document.querySelector('.p');
+    const btns=document.querySelectorAll('.calc-button');
+    btns.forEach(btn=>{
+        btn.addEventListener('click',()=>{
+            const type=btn.dataset.type;
+            const value=btn.textContent;
+            if(type=="number"){
+                if(operator===""){
+                firstNumber+=value;
+                display.textContent=firstNumber}
+                else{
+                    secondNumber+=value;
+                    display.textContent=secondNumber;
+                }
+
+            }
+            if(type=="operator"){
+                if(firstNumber===""){
+                    alert("first enter a number");
+                }
+                operator=value;
+                display.textContent=operator;
+            }
+
+            if(type=="equals"){
+                if(firstNumber!="" && operator!="" && secondNumber!=""){
+                    const result=operate(operator,firstNumber,secondNumber);
+                    display.textContent=result;
+                    firstNumber=result.toString();
+                    operator="";
+                    secondNumber="";
+                }
+            }
+            if(type=="clear"){
+                firstNumber="";
+                secondNumber="";
+                operator="";
+                display.textContent="";
+            }
+
+        });
+        
+    });
+    
+    
+   
+}
+
+work();
